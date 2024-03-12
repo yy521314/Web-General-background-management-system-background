18646c3de7863246cfa47f1b14e6a3b33837fced
yang <1173278084@qq.com>
2024-03-12 16:58:20 +0800

    完善样式

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: 'yang' '1173278084@qq.com'
  * @Date: 2024-03-10 22:37:20
  * @LastEditors: 'yang' '1173278084@qq.com'
- * @LastEditTime: 2024-03-11 21:58:33
+ * @LastEditTime: 2024-03-12 15:18:55
  * @FilePath: \Web-General-background-management-system-background\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -11,4 +11,9 @@
 	<RouterView />
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+:deep(body) {
+	margin: 0;
+	padding: 0;
+}
+</style>
