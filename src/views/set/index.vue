26848784c364e0dcaa0fec17d8be768bdc727cff
yang <1173278084@qq.com>
2024-03-14 23:31:30 +0800

    Revert "完善header栏"
    This reverts commit ac0d8356746bf2e01ad98d6e68f8b3038c263711.

--- src/views/set/index.vue
+++ src/views/set/index.vue
@@ -2,7 +2,7 @@
  * @Author: 'yang' '1173278084@qq.com'
  * @Date: 2024-03-13 14:48:23
  * @LastEditors: 'yang' '1173278084@qq.com'
- * @LastEditTime: 2024-03-14 23:18:40
+ * @LastEditTime: 2024-03-14 19:20:51
  * @FilePath: \Web-General-background-management-system-background\src\views\set\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
